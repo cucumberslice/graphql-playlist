27148e9e5c326bae47c9a962f7ba85e44c19a876
cucmberslice <ywblackberry@gmail.com>
2019-03-11 21:16:41 -0400

    added author and book relationship in the Booktype

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,9 +7,9 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} 
 //dummy data
 
 let books  = [
-    { name:'Name of the Wine', genre:'Fantasy', id:'1'},
-    { name:'The Final Empire', genre:'Fantasy', id:'2'},
-    { name: 'The Long Earth',  genre:'Sci-Fi',id: '3'}
+    { name:'Name of the Wine', genre:'Fantasy', id:'1', authorid:'1'},
+    { name:'The Final Empire', genre:'Fantasy', id:'2', authorid:'2'},
+    { name: 'The Long Earth',  genre:'Sci-Fi',id: '3', authorid:'3'}
 ]
 
 let authors = [
@@ -31,7 +31,13 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID},
         name: { type: GraphQLString},
-        genre: { type: GraphQLString}
+        genre: { type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                return _.find(authors, {id:parent.authorid})
+            }
+        }
 
     })
 })
